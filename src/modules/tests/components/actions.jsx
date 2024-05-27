0c336dd11d2e7d6572c05a5f7501d5b19d3c1f77
Ghukasyan <hovhannes.ghukasyan@ekeng.am>
2024-05-27 11:48:44 +0400

    Navigation from tests to test

--- src/modules/tests/components/actions.jsx
+++ src/modules/tests/components/actions.jsx
@@ -1,7 +1,9 @@
 import PropTypes from "prop-types";
 import Button from '../../../ui/button';
 import { Play } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 const Actions = ({ test }) => {
+    const navigate = useNavigate();
     const currentDateTime = new Date();
     const canStartTest = new Date(test.test_data_from) <= currentDateTime && new Date(test.test_data_to) >= currentDateTime;
     const { status } = test;
@@ -10,7 +12,7 @@ const Actions = ({ test }) => {
         <div className="flex items-center gap-2">
             {canStartTest && status == "pending" ? (
                 <div>
-                    <Button className="flex items-center justify-center gap-2">
+                    <Button className="flex items-center justify-center gap-2" onClick={() => navigate('/test/' + test.id)}>
                         <Play className="w-4 h-4" />
                         Start Test
                     </Button>
