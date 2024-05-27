b6a4dd603701b66554b640b50daf692f6bfa0f61
Ghukasyan <hovhannes.ghukasyan@ekeng.am>
2024-05-27 11:33:13 +0400

    Sidebar getting from constant with mapping

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,13 +1,25 @@
-const Sidebar = () => {
+import { icons } from "lucide-react";
+import sidebar from "../../constants/sidebar";
 
+const Sidebar = () => {
+    const pathname = window.location.pathname;
     return (
         <div className="border-r border-lightgray h-full sticky top-0">
             <div className="bg-white w-64 h-full">
                 <div className="p-3">
                     <ul className="space-y-4">
-                        <li>
-                            <a href="/tests" className={`transition-all duration-75 w-full flex items-center gap-2 hover:bg-gray-500/10 p-2 rounded-lg`}>Tests</a>
-                        </li>
+                        {sidebar.map((item) => {
+                            const isActive = pathname === item.path;
+                            const Icon = icons[item.icon];
+                            return (
+                                <li key={`sidebar-item-${item.path}`}>
+                                    <a href={item.path} className={`transition-all duration-75 w-full flex items-center gap-2 hover:bg-gray-500/10 p-2 rounded-lg ${isActive && "bg-gray-500/10 text-blue-500"}`}>
+                                        <Icon size="1.25em" name={item.icon} />
+                                        {item.title}
+                                    </a>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
             </div>
